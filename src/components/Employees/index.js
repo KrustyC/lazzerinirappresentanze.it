e5bd9079eb5347cd56e4fe95a6b82722b72ff033
Davide Crestini <davide.crestini@theculturetrip.com>
2020-05-09 18:34:04 +0100

    updated employees

--- src/components/Employees/index.js
+++ src/components/Employees/index.js
@@ -1,39 +1,57 @@
 import React from "react"
 import { EmployeesContainer, Employee } from "./Employees.styled"
 
-const employees = [
-  {
-    name: "Maria Rossi",
-    job: "Assistant Sales Manager",
-    picture:
-      "https://www.rd.com/wp-content/uploads/2017/09/01-shutterstock_476340928-Irina-Bg.jpg",
-  },
-  {
-    name: "Luca Lazzerini",
-    job: "Sales Manager",
-    picture:
-      "https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcQFGxEA_sP1TrD6MGIDHAza-VAP1qYdB46ERfTxnd7vTJ9ey0Us&usqp=CAU",
-  },
-  {
-    name: "John Doe",
-    job: "Sales Manager",
-    picture:
-      "https://cdn.fastly.picmonkey.com/contentful/h6goo9gw1hh6/6R0kusgfZmQv1jNcHSL9GU/f4c9517962f99704c3a0df849061b380/linkedIN-profile-FB.jpg",
-  },
-]
+// const employees = [
+//   {
+//     name: "Maria Rossi",
+//     job: "Assistant Sales Manager",
+//     picture:
+//       "https://www.rd.com/wp-content/uploads/2017/09/01-shutterstock_476340928-Irina-Bg.jpg",
+//   },
+//   {
+//     name: "Luca Lazzerini",
+//     job: "Sales Manager",
+//     picture:
+//       "https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcQFGxEA_sP1TrD6MGIDHAza-VAP1qYdB46ERfTxnd7vTJ9ey0Us&usqp=CAU",
+//   },
+//   {
+//     name: "John Doe",
+//     job: "Sales Manager",
+//     picture:
+//       "https://cdn.fastly.picmonkey.com/contentful/h6goo9gw1hh6/6R0kusgfZmQv1jNcHSL9GU/f4c9517962f99704c3a0df849061b380/linkedIN-profile-FB.jpg",
+//   },
+// ]
 
-export const Employees = () => (
-  <EmployeesContainer>
-    {employees.map(({ name, job, picture }) => (
-      <Employee>
-        <Employee.Container>
-          <Employee.Pic>
-            <Employee.Pic.Image urlSrc={picture} />
-          </Employee.Pic>
-          <Employee.Name>{name}</Employee.Name>
-          <Employee.Job>{job}</Employee.Job>
-        </Employee.Container>
-      </Employee>
-    ))}
-  </EmployeesContainer>
-)
+export const Employees = () => {
+  const employees = []
+
+  const data = useStaticQuery(graphql`
+    query GetEmployees {
+      markdownRemark(fileAbsolutePath: { regex: "/home/" }) {
+        frontmatter {
+          title
+          introduction
+          ourHistory1
+          ourHistory2
+        }
+      }
+    }
+  `)
+  
+  return (
+    <EmployeesContainer>
+      {employees.map(({ name, job, picture }) => (
+        <Employee>
+          <Employee.Container>
+            <Employee.Pic>
+              <Employee.Pic.Image urlSrc={picture} />
+            </Employee.Pic>
+            <Employee.Name>{name}</Employee.Name>
+            <Employee.Job>{job}</Employee.Job>
+          </Employee.Container>
+        </Employee>
+      ))}
+    </EmployeesContainer>
+  )
+}
+
--- src/components/Form/index.js
+++ src/components/Form/index.js
@@ -1,6 +1,5 @@
 import React from "react"
 import { Link } from "gatsby"
-import { ErrorMessage } from 'formik'
 import { CenteredColumn } from "../CenteredColumn"
 import { useContactForm } from "../../hooks/useContactForm"
 import {
